062fe6d50dbd1eb7c0a3a6c527429d846ece1fbc
MoniZuluagaP <monicazul.desarrollo@gmail.com>
2024-06-05 00:21:25 -0300

    Elimine carpetas de conflicto

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const connection = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'Estudiar2024',
+  password: '',
   database: 'idw',
   waitForConnections: true,
   connectionLimit: 10,
